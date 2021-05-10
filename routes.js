514837246ad0e9eb0b7a6ca74594dd6396b7b1be
Caique Rodrigues <caiquerodrigues@id.uff.br>
2021-05-10 13:31:00 -0300

    working with forms, http, backend

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = express.Router()
+const teachers = require('./teachers')
 
 routes.get('/', (req, res) => {
     return res.redirect('teachers')
@@ -12,6 +13,12 @@ routes.get('/', (req, res) => {
     .get('/students', (req,res)=>{
         return res.render('/students')
     })
+
+    .get('/teachers/create', (req, res)=>{
+        return res.render('teachers/create')
+    })
+
+    .post('/teachers', teachers.post)
 
 
 
