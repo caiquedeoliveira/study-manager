1ead9b617b46a7a8562ae41649b8e22257d184c0
Caique Rodrigues <caiquerodrigues@id.uff.br>
2021-05-24 10:59:13 -0300

    CRUD for the students profiles

--- src/app/controllers/students.js
+++ src/app/controllers/students.js
@@ -1,7 +1,21 @@
+const { schooling, date } = require('../../lib/utils')
+const Student = require('../../models/Student')
+
+
 module.exports = {
     index(req,res){
-        return res.render('students/index')
-
+        Student.all(allStudents => {
+            
+            const students = allStudents.map(student => {
+                let newStudent = {
+                    ...student,
+                    school_year: schooling(student.school_year)
+                }
+                return newStudent
+            })
+            
+            return res.render('students/index', {students})
+        })
     },
     create(req,res){
         return res.render('students/create')
@@ -15,19 +29,33 @@ module.exports = {
                 return res.send('Please, fill all the fields!')
             }
         }
-    
-        let {avatar_url, name, birth, schooling, classtype, services} = req.body
 
-        return
+        Student.create(req.body, student => {
+            return res.redirect(`/students/${student.id}`)
+        })
 
     },
     show(req,res){
-        return
+        Student.find(req.params.id, student => {
+            if(!student) return res.send('Student not found')
+
+            student.birth_date = date(student.birth_date).birthDay
+            student.school_year = schooling(student.school_year)
+            student.created_at = date(student.created_at).format
+
+            return res.render('students/show', {student})
+        })
 
     },
     edit(req,res){
+        Student.find(req.params.id, student => {
+            if(!student) return res.send("Student not found!")
 
-        return
+            student.birth_date = date(student.birth_date).iso
+
+            return res.render("students/edit", {student})
+        })
+        
     },
     put(req,res){
 
@@ -39,12 +67,15 @@ module.exports = {
             }
         }
     
-
-        return
+        Student.update(req.body, ()=>{
+            return res.redirect(`/students/${req.body.id}`)
+        })
 
     },
     delete(req,res){
-        return
+        Student.delete(req.body.id, ()=>{
+            return res.redirect('/students')
+        })
 
     },
 }
--- src/models/Student.js
+++ src/models/Student.js
@@ -3,7 +3,7 @@ const db = require('../config/db')
 
 module.exports = {
     all(callback){
-        db.query(`SELECT * FROM teachers ORDER BY name ASC`, (err, results)=>{
+        db.query(`SELECT * FROM students ORDER BY name ASC`, (err, results)=>{
             if(err) throw `Database error ${err}`
 
             callback(results.rows)
@@ -11,26 +11,24 @@ module.exports = {
     },
     create(data, callback){
         const query = `
-            INSERT INTO teachers (
+            INSERT INTO students (
                 avatar_url,
                 name,
+                email,
                 birth_date,
-                education_level,
-                class_type,
-                subjects_taught,
-                created_at
-            ) VALUES ($1, $2, $3, $4, $5, $6, $7)
+                school_year,
+                workload
+            ) VALUES ($1, $2, $3, $4, $5, $6)
             RETURNING id
         `
 
         const values = [
             data.avatar_url,
             data.name,
+            data.email,
             date(data.birth_date).iso,
-            data.education_level,
-            data.class_type,
-            data.subjects_taught,
-            date(Date.now()).iso
+            data.school_year,
+            data.workload
         ]
 
         db.query(query, values, (err, results)=>{
@@ -40,7 +38,7 @@ module.exports = {
         })
     },
     find(id, callback){
-        db.query(`SELECT * FROM teachers WHERE id = $1`, [id], (err, results)=>{
+        db.query(`SELECT * FROM students WHERE id = $1`, [id], (err, results)=>{
             if(err) throw `Database error! ${err}`
 
             callback(results.rows[0])
@@ -48,23 +46,23 @@ module.exports = {
     },
     update(data, callback){
         const query = `
-            UPDATE teachers SET
+            UPDATE students SET
                 avatar_url=($1),
                 name=($2),
-                birth_date=($3),
-                education_level=($4),
-                class_type=($5),
-                subjects_taught=($6)
+                email=($3),
+                birth_date=($4),
+                school_year=($5),
+                workload=($6)
             WHERE id = $7
         `
 
         const values = [
             data.avatar_url,
             data.name,
+            data.email,
             date(data.birth_date).iso,
-            data.education_level,
-            data.class_type,
-            data.subjects_taught,
+            data.school_year,
+            data.workload,
             data.id
         ]
 
@@ -74,7 +72,7 @@ module.exports = {
         })
     },
     delete(id, callback){
-        db.query(`DELETE FROM teachers WHERE id = $1`, [id], (err, results)=>{
+        db.query(`DELETE FROM students WHERE id = $1`, [id], (err, results)=>{
             if(err) throw `Database error! ${err}`
 
             return callback()
