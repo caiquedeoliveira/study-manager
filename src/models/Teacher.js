cf3a6594e3f23f02b07d7ead9d6ae3d6f32a188b
Caique Rodrigues <caiquerodrigues@id.uff.br>
2021-06-03 19:15:51 -0300

    css changes

--- src/models/Teacher.js
+++ src/models/Teacher.js
@@ -118,14 +118,14 @@ module.exports = {
         }
 
         query = `
-            SELECT teachers.*, ${totalQuery}, count(students) AS total_students
-            FROM teachers
-            LEFT JOIN students ON (teachers.id = students.teacher_id)
-            ${filterQuery}
-            GROUP BY teachers.id LIMIT $1 OFFSET $2
-        `
+        SELECT teachers.*, ${totalQuery} , count(students) AS total_students
+        FROM teachers
+        LEFT JOIN students ON (teachers.id = students.teacher_id)
+        ${filterQuery}
+        GROUP BY teachers.id LIMIT $1 OFFSET $2
+    `
 
-        db.query(query, [limit, offset], (err, results)=>{
+        db.query(query, [limit, offset], (err, results)=> {
             if(err) throw `Database error! ${err}`
 
             callback(results.rows)
